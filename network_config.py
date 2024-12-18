0c35b8e589e3294b8332bdaa1661617b213ac393
KNTOLIJO <108604756+adhe222@users.noreply.github.com>
2024-12-17 19:09:34 -0800

    Update network_config.py

--- network_config.py
+++ network_config.py
@@ -7,7 +7,7 @@ networks = {
         'contract_address': '0x8D86c3573928CE125f9b2df59918c383aa2B514D'
     },
     'OP Sepolia': {
-        'rpc_url': 'https://optimism-sepolia.blockpi.network/v1/rpc/public',
+        'rpc_url': 'https://optimism-sepolia.drpc.org',
         'chain_id': 11155420,
         'contract_address': '0xF221750e52aA080835d2957F2Eed0d5d7dDD8C38'
     },
